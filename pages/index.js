7d8c71ac2a14da47ad6343284cb498b9599f2f11
bweinfeld5 <128821172+bweinfeld5@users.noreply.github.com>
2024-05-21 11:49:50 -0400

    Index fix

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,7 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Scholar Scope</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.coverPage}>
-        <h1 className={styles.title}>Scholar Scope</h1>
-      </main>
-    </div>
-  )
-}
-import Head from 'next/head'
 import Header from '@components/Header'
+import Footer from '@components/Footer'
+import CollegeLogos from '@components/CollegeLogos'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -29,8 +14,12 @@ export default function Home() {
 
       <main className={styles.coverPage}>
         <Header title="Scholar Scope" />
+        <CollegeLogos />
       </main>
+
+      <Footer />
     </div>
   )
 }
+
 

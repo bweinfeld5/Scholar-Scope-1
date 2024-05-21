78be83fef4b24abbaadef41372d5f74b0cbda69d
bweinfeld5 <128821172+bweinfeld5@users.noreply.github.com>
2024-05-21 11:40:15 -0400

    index fix

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,6 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to Scholar Scope!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
-// pages/index.js
-
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
 import CollegeLogos from '@components/CollegeLogos'
 import styles from '../styles/Home.module.css'
 
@@ -46,3 +21,4 @@ export default function Home() {
     </div>
   )
 }
+

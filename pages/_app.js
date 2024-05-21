bb99d995d4ed7b369f957cc9ae41dd923ea258d2
bweinfeld5 <128821172+bweinfeld5@users.noreply.github.com>
2024-05-21 19:17:27 -0400

    try now

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,3 @@
-import '@styles/globals.css'
-
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default Application
 // pages/_app.js
 import Layout from '../components/Layout';
 import '../styles/globals.css';

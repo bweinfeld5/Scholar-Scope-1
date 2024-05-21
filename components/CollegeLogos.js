6ed1d8366b0f02ccef836b46beb2dac5e40fa722
bweinfeld5 <128821172+bweinfeld5@users.noreply.github.com>
2024-05-21 12:37:40 -0400

    new image 2

--- components/CollegeLogos.js
+++ components/CollegeLogos.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css';
 const logos = [
   '/public/Logos/BClogo.png',
   '/public/Logos/Emorylogo.png',
-  '/public/Logos/Wakelogo.png',
+  '/https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Wake_Forest_University_Athletic_logo.svg/2560px-Wake_Forest_University_Athletic_logo.svg.png',
   // Add paths to your college logos here
 ];
 

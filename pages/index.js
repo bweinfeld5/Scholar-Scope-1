de9a58c0495642909830adbf52de6c85fe972f4e
bweinfeld5 <128821172+bweinfeld5@users.noreply.github.com>
2024-05-21 11:36:13 -0400

    Update 2

--- pages/index.js
+++ pages/index.js
@@ -21,3 +21,28 @@ export default function Home() {
     </div>
   )
 }
+// pages/index.js
+
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+import CollegeLogos from '@components/CollegeLogos'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Scholar Scope</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.coverPage}>
+        <h1 className={styles.title}>Scholar Scope</h1>
+        <CollegeLogos />
+      </main>
+
+      <Footer />
+    </div>
+  )
+}

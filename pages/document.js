d50ffec34480e7bf6f496df7c8798906ea67bbb8
bweinfeld5 <128821172+bweinfeld5@users.noreply.github.com>
2024-05-21 19:56:15 -0400

    go

---
+++ pages/document.js
@@ -0,0 +1,20 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="stylesheet" href="https://use.typekit.net/your-kit-id.css" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;

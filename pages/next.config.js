846d7d98cba3a8a4cc180f78c06c22f82f18e019
bweinfeld5 <128821172+bweinfeld5@users.noreply.github.com>
2024-05-22 00:33:07 -0400

    Fix jsconfig.json location and ensure correct configuration

--- pages/next.config.js
+++ pages/next.config.js
@@ -1,24 +1,25 @@
 module.exports = {
-    future: {
-      webpack5: true,
-    },
-    webpack: (config, { dev, isServer }) => {
-      if (!dev && !isServer) {
-        config.cache = {
-          type: 'filesystem',
-        };
-      }
-      config.module.rules.push({
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-            },
+  future: {
+    webpack5: true,
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.cache = {
+        type: 'filesystem',
+      };
+    }
+    config.module.rules.push({
+      test: /\.css$/,
+      exclude: /use\.typekit\.net/,
+      use: [
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1,
           },
-        ],
-      });
-      return config;
-    },
-  };
+        },
+      ],
+    });
+    return config;
+  },
+};
